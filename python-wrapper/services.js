01d75b401fb122e0baeffdb08578cf63bd5f237e
Valerio Lomanto <ian.maayrkas@gmail.com>
2023-03-06 11:15:14 +0100

    remove outdated examples

--- python-wrapper/services.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cmdService = void 0;
-const cmd_1 = require("twinica-lib/services/cmd");
-const child_process_1 = require("child_process");
-exports.cmdService = new cmd_1.CmdService(child_process_1.spawn);
-//# sourceMappingURL=services.js.map
